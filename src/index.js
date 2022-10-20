f61b6a95e15196d0d027bfba90c7b69930ce0169
Eric Butler <72872237+EricDeBuitleir@users.noreply.github.com>
2022-10-20 12:37:54 +0100

    Refactor - useMovie custom hook.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import {Link} from 'react-router-dom'
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -18,10 +19,11 @@ const App = () => {
         </li>
       </ul>
       <Routes>
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="*" element={ <Navigate to="/" /> } /> 
       </Routes>
     </BrowserRouter>
   );

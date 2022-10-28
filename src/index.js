162097ea96e7bbe29fc75c52b2a7e246c3821511
Eric Butler <72872237+EricDeBuitleir@users.noreply.github.com>
2022-10-28 20:34:12 +0100

    Using react-query to manage the browser-based cache

--- src/index.js
+++ src/index.js
@@ -9,24 +9,39 @@ import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import UpcomingMovies from './pages/upcomingMoviesPage';
 import {Link} from 'react-router-dom'
 import MovieReviewPage from "./pages/movieReviewPage";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import MoviesContextProvider from "./contexts/moviesContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 const App = () => {
   return (
-  
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <SiteHeader />      {/* New Header  */}
-      <Routes>
-        <Route path="/reviews/:id" element={<MovieReviewPage />} />
-        <Route path="/details/:id" element={<MovieDetailsPage />} />
-        <Route path="/movies/upcoming" element={<UpcomingMovies/>} />
-        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
-    
-  );
+    <SiteHeader />      {/* New Header  */}
+    <Routes>
+      <Route path="/reviews/:id" element={<MovieReviewPage />} />
+      <Route path="/details/:id" element={<MovieDetailsPage />} />
+      <Route path="/movies/upcoming" element={<UpcomingMovies/>} />
+      <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+      <Route path="/movies/:id" element={<MoviePage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </BrowserRouter>
+  <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  
+);
 };
 
 const rootElement = createRoot( document.getElementById("root") )

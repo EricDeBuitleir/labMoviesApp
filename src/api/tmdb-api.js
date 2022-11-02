174183e343a65595be6eff7d9b568b520bc3a62f
Eric Butler <72872237+EricDeBuitleir@users.noreply.github.com>
2022-11-02 12:24:20 +0000

    "New button for upcoming movies page."

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -75,10 +75,13 @@ export const getMovie = (args) => {
   export const getUpcomingMovies = (up) => {
     return fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=${up}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
-        return json.results;
-      });
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
   };
--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,22 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import MustWatch from '../components/cardIcons/addMustWatch';
 
 const UpcomingMovies = (props) => {
-  const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
 
   
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
+      action={(movie) => {
+        return <MustWatch movie={movie} />
+      }}
     />
   );
 };
